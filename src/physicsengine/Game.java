83c665780082fed35059341a8d933b66832ce68f
nkshv <nikrishov@yandex.com>
2024-02-21 18:03:59 -0300

    hashmap for generalization

--- src/physicsengine/Game.java
+++ src/physicsengine/Game.java
@@ -1,39 +1,36 @@
 package physicsengine;
 import org.newdawn.slick.*;
+import java.util.*;
 
 public class Game extends BasicGame{
-   
-    private static Vector v1,v2,v3,v4,v5;
-    private static Circle c1,c2,c3,c4,c5;
-    
-    private Image a,b,c,d,e;
-    private Sound oof, thud;
-    private float x1,x2,x3,x4,x5; //NOT THE CENTER OF THE CIRCLE
-    private float y1,y2,y3,y4,y5; //NOT THE CENTER OF THE CIRCLE
     private int numberOfCircles = 0;
     public static float mass, radius;
-    
-    private float Yvelocity1,Yvelocity2,Yvelocity3,Yvelocity4,Yvelocity5;
-    private float Xvelocity1, Xvelocity2, Xvelocity3, Xvelocity4,Xvelocity5;
-    
-    private Image[] Image_Array;
-    
 
+    static List<Image> imageList = new ArrayList<>();
+    static HashMap<Integer, List<Object>> hashMap = new HashMap<>();
+  
     @Override
     public void render(GameContainer gc, Graphics g) throws SlickException{
-        
-        float[][] allPositions = {{getX(0), getY(0)}, {getX(1), getY(1)}, {getX(2), getY(2)}, {getX(3), getY(3)}, {getX(4), getY(4)}};
-        float[] radii = {c1 != null ? c1.getRadius() : 0, c2 != null ? c2.getRadius() : 0, c3 != null ? c3.getRadius() : 0, c4 != null ? c4.getRadius() : 0, c5 != null ? c5.getRadius() : 0};        // FREE FALL + HORIZONTAL MOVEMENT IMPLEMENTATION
-
         int collisionCount = 0;
-
+        
         for(int i=0; i<numberOfCircles; i++){
-            Image_Array[i].draw(getX(i), getY(i)); // RENDER BALLS
+            float x = (float) hashMap.get(i).get(0);    float y = (float) hashMap.get(i).get(1);
+            System.out.println(x);
+            //System.out.println("X: " + x + "   Y: " + y);
+            imageList.get(i).draw(x, y); // RENDER BALLS
             for (int j = i + 1; j < numberOfCircles; j++) {
-                float[] position1 = {allPositions[i][0], allPositions[i][1]};
-                float[] position2 = {allPositions[j][0], allPositions[j][1]};
+                float x1 = (float) hashMap.get(i).get(0);   float y1 = (float) hashMap.get(i).get(1);
+                float xv1 = (float) hashMap.get(i).get(2);  float yv1 = (float) hashMap.get(i).get(3);
+                float radius1 = (float) hashMap.get(i).get(4);
+                
+                float x2 = (float) hashMap.get(j).get(0);   float y2 = (float) hashMap.get(j).get(1);
+                float xv2 = (float) hashMap.get(j).get(2);  float yv2 = (float) hashMap.get(j).get(3);
+                float radius2 = (float) hashMap.get(j).get(4);
+                        
+                float[] position1 = {x1, y1};   float[] position2 = {x2, y2};
+                
 
-                if (checkCollision(position1, radii[i], position2, radii[j])){
+                if (checkCollision(position1, radius1, position2, radius2)){
                     int yCoordinate = 50 + (collisionCount * 20); // for drawString purposes
                     g.drawString("COLLISION HAPPENED BETWEEN " + (i + 1) + " AND " + (j + 1) + "!", 100, yCoordinate);
                     collisionCount++; // Increment the collision count
@@ -43,131 +40,68 @@ public class Game extends BasicGame{
     }
     @Override
     public void update(GameContainer gc, int t) throws SlickException {
+                
         float delta = t /60.f; // converts t to seconds
-        float[] xPositions = {x1, x2, x3, x4, x5};
-        float[] yPositions = {y1, y2, y3, y4, y5};
-        float[] Xvelocities = {Xvelocity1, Xvelocity2, Xvelocity3, Xvelocity4, Xvelocity5};
-        float[] Yvelocities = {Yvelocity1, Yvelocity2, Yvelocity3, Yvelocity4, Yvelocity5};
-        float[] radii = {c1 != null ? c1.getRadius() : 0, c2 != null ? c2.getRadius() : 0, c3 != null ? c3.getRadius() : 0, c4 != null ? c4.getRadius() : 0, c5 != null ? c5.getRadius() : 0};        // FREE FALL + HORIZONTAL MOVEMENT IMPLEMENTATION
+
         for (int i = 0; i < numberOfCircles; i++) {
-            float[] result_y = freefall(radii[i], yPositions[i], Yvelocities[i], delta, Forces.MOON_GRAVITY);
-            float[] result_x = horizontalMove(radii[i], xPositions[i], Xvelocities[i], delta);
-        
-            yPositions[i] = result_y[0]; Yvelocities[i] = result_y[1];
-            xPositions[i] = result_x[0]; Xvelocities[i] = result_x[1];
+            float x = (float) hashMap.get(i).get(0);    float y = (float) hashMap.get(i).get(1);
+            float xv = (float) hashMap.get(i).get(2);   float yv = (float) hashMap.get(i).get(3);
+            float radius = (float) hashMap.get(i).get(4);
+            
+            float[] result_y = freefall(radius, y, yv, delta, Forces.EARTH_GRAVITY);
+            float[] result_x = horizontalMove(radius, x, xv, delta);
+                        
+            List<Object> values = new ArrayList<>();
+            values.add(result_x[0]);    values.add(result_y[0]);
+            values.add(result_x[1]);    values.add(result_y[1]);
+            values.add(radius);
+            
+            hashMap.put(i, values);
     }
-        y1 = yPositions[0]; y2 = yPositions[1]; y3 = yPositions[2]; y4 = yPositions[3]; y5 = yPositions[4];
-        x1 = xPositions[0]; x2 = xPositions[1]; x3 = xPositions[2]; x4 = xPositions[3]; x5 = xPositions[4];
-        
-        Yvelocity1 = Yvelocities[0]; Yvelocity2 = Yvelocities[1]; Yvelocity3 = Yvelocities[2]; Yvelocity4 = Yvelocities[3]; Yvelocity5 = Yvelocities[4];
-        Xvelocity1 = Xvelocities[0]; Xvelocity2 = Xvelocities[1]; Xvelocity3 = Xvelocities[2]; Xvelocity4 = Xvelocities[3]; Xvelocity5 = Xvelocities[4];
             }
     @Override
     public void init(GameContainer gc) throws SlickException{
-        a = initializeCircle(0, 0, 14, 0, 100); // (X, Y, X-VELOCITY, Y-VELOCITY, RADIUS)
-        b = initializeCircle(222, 0, 20, 0, 30);
-        c = initializeCircle(600, 0, 30, 0, 25);
-        //d = initializeCircle(900, 0, 2, 0, 5);
-        //e = initializeCircle(1200, 0, 1, 0, 5);
-
-        Image_Array = new Image[]{a, b, c, d, e};
+        initializeCircle(0, 0, 14, 0, 56); // (X, Y, X-VELOCITY, Y-VELOCITY, RADIUS)
+        initializeCircle(222, 0, 20, 0, 56);
+        initializeCircle(600, 0, 30, 0, 56);
+        initializeCircle(900, 0, 20, 0, 56);
+        initializeCircle(1200, 0, 24, 0, 56);
+        initializeCircle(700, 0, 30, 0, 56);
     }
    
-  
     private Image initializeCircle(float x, float y, float xv, float yv, float radius) {
-        Image image = null;
-        try {
-            switch (numberOfCircles) {
-                case 0:
-                    x1 = x;
-                    y1 = y;
-                    Yvelocity1 = yv;
-                    Xvelocity1 = xv;
-                    image = new Image("assets/ball1.png");
-                    image = image.getScaledCopy(radius/112.0f); //ball scaled to radius "radius"
-                    c1 = new Circle(new Vector(x, y), mass, radius, xv, yv);
-                    break;
-                case 1:
-                    x2 = x;
-                    y2 = y;
-                    Yvelocity2 = yv;
-                    Xvelocity2 = xv;
-                    image = new Image("assets/ball2.png");
-                    image = image.getScaledCopy(radius/112.0f);
-                    c2 = new Circle(new Vector(x, y), mass, radius, xv, yv);
-                    break;
-                case 2:
-                    x3 = x;
-                    y3 = y;
-                    Yvelocity3 = yv;
-                    Xvelocity3 = xv;
-                    image = new Image("assets/ball3.png");
-                    image = image.getScaledCopy(radius/112.0f);
-                    c3 = new Circle(new Vector(x, y), mass, radius, xv, yv);
-                    break;
-                case 3:
-                    x4 = x;
-                    y4 = y;
-                    Yvelocity4 = yv;
-                    Xvelocity4 = xv;
-                    image = new Image("assets/ball1.png");
-                    image = image.getScaledCopy(radius/112.0f);
-                    c4 = new Circle(new Vector(x, y), mass, radius, xv, yv);
-                    break;
-                case 4:
-                    x5 = x;
-                    y5 = y;
-                    Yvelocity5 = yv;
-                    Xvelocity5 = xv;
-                    image = new Image("assets/ball1.png");
-                    image = image.getScaledCopy(radius/112.0f);
-                    c5 = new Circle(new Vector(x, y), mass, radius, xv, yv);
-                    break;
-                default:
-                    System.out.println("No image defined for this case!");
-                    break;
-            }
-        } catch (SlickException e) {
-            e.printStackTrace();
-        }
-        numberOfCircles++;
-        return image;
-    }
-   
-    //change it to an array so that it returns every useful variable
-    public float getY(int circleIndex) {
-        switch (circleIndex) {
-            case 0:
-                return y1;
-            case 1:
-                return y2;
-            case 2:
-                return y3;
-            case 3:
-                return y4;
-            case 4:
-                return y5;
-            default:
-                return Float.NaN;
-        }
-    }
-    public float getX(int circleIndex) {
-    switch (circleIndex) {
-        case 0:
-            return x1;
-        case 1:
-            return x2;
-        case 2:
-            return x3;
-        case 3:
-            return x4;
-        case 4:
-            return x5;
-        default:
-            return Float.NaN; 
-    }
-}   
-    
+      Image image = null;
+      try {
+          List<Object> values = new ArrayList<>();
+          values.add(x);
+          values.add(y);
+          values.add(xv);
+          values.add(yv);
+          values.add(radius);
+          hashMap.put(numberOfCircles, values);
+          
+          if ((numberOfCircles + 1) <=5){
+            String filePath = "assets" + "/ball" + (numberOfCircles + 1) + ".png";
+            image = new Image(filePath);
+            image = image.getScaledCopy(radius / 112.0f);
+            imageList.add(image);
+            System.out.println((numberOfCircles + 1) + "   -  " + filePath);
+          }
+          else{
+            String filePath = "assets" + "/ball" + ".png";
+            image = new Image(filePath);
+            image = image.getScaledCopy(radius / 112.0f);
+            imageList.add(image);
+            System.out.println((numberOfCircles + 1) + "   -  " + filePath);
+          }
+      } catch (SlickException e) {
+          e.printStackTrace();
+      }
+      numberOfCircles++;
+      return image;
+  }
+
+        
     public float[] freefall(float radius, float y, float Yvelocity, float delta, float GRAVITY) {
         if (y >= 768 - 2*radius ) { 
             Yvelocity *= -1;
@@ -205,7 +139,6 @@ public class Game extends BasicGame{
 
             if (distance <= radius1 + radius2){
                 collision = true;
-                System.out.println(distance + " --- " + h + "----" + b);
             }
 
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -215,13 +148,25 @@ public class Game extends BasicGame{
     }
     
     public float[] collisionResolution(float[] position1, float Xv, float mass1, float[] position2, float Yv, float mass2){
-        float x1 = position1[0]; y1 = position1[1];
-        float x2 = position2[1]; y2 = position2[1];
+        float x1 = position1[0]; float y1 = position1[1];
+        float x2 = position2[1]; float y2 = position2[1];
         
         // -> x1, y1, x2, y2, Xv, Yv, mass1, mass2 <-
         
         
         return new float[]{};
+    }
+        private static void printHashMap() {
+        for (Map.Entry<Integer, List<Object>> entry : hashMap.entrySet()) {
+            Integer key = entry.getKey();
+            List<Object> values = entry.getValue();
+            System.out.print("Key: " + key + ", ");
+            System.out.print("Values: ");
+            for (Object value : values) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        } 
     }
 
     public Game(){
--- src/physicsengine/PhysicsEngine.java
+++ src/physicsengine/PhysicsEngine.java
@@ -11,16 +11,7 @@ public class PhysicsEngine {
     private static Vector v4;
 
     public static void main(String[] args) {
-        v1 = new Vector(0, 10);
-        v2 = new Vector(10, 0);
-        v3 = new Vector();
-        v4 = new Vector(5, 5);
 
-
-        Circle c1 = new Circle(v2, 5, 5, 0, 0);
-        
-        System.out.println(c1.getX());
-        System.out.println(c1.getY());
 
         try {
             AppGameContainer app = new AppGameContainer(new Game());
